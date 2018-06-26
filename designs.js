0967aacbeb4d9d40b967a9cc87c2aa1edefb84f7
Theodore Olanipekun Akinyede <olanipekun.akinyede@ucdconnect.ie>
2018-06-26 15:20:47 +0100

    declared variables using jQuery

--- designs.js
+++ designs.js
@@ -2,9 +2,21 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+$('body').append("<h1 class = 'bob'>try</h1>");
+
+var height = $('#inputHeight').val();
+var weight = $('#inputWeight').val();
+var table = $('#pixelCanvas');
+var color = $('#colorPicker').val();
+var submitButton = $('input[type="submit"]');
 
 function makeGrid() {
 
 // Your code goes here!
 
 }
+
+// submitButton.on('click', function (e) {
+//     e.preventDefault();
+//         brr();
+//     });
