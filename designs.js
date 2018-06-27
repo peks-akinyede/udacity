41fe898f7ba827ad19d6a62bf10f9a305c9b3b32
Theodore Olanipekun Akinyede <olanipekun.akinyede@ucdconnect.ie>
2018-06-27 11:25:27 +0100

    add document.ready() jQuery

--- designs.js
+++ designs.js
@@ -2,40 +2,44 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-var table = $('#pixelCanvas');
-var submitButton = $('input[type="submit"]');
 
-submitButton.on('click', function (e) {
-  e.preventDefault();
-  makeGrid();
-});
+$(document).ready(function(){
+  var table = $('#pixelCanvas');
+  var submitButton = $('input[type="submit"]');
 
-function makeGrid() {
-  var height = $('#inputHeight').val();
-  var weight = $('#inputWeight').val();
-  $('tbody').remove();
+  submitButton.on('click', function (e) {
+    e.preventDefault();
+    makeGrid();
+  });
 
-  table.append('<tbody></tbody>');
-  let tableBody = table.find('tbody');
+  function makeGrid() {
+    var height = $('#inputHeight').val();
+    var weight = $('#inputWeight').val();
+    $('tbody').remove();
 
-  for(let i =0; i<height;i++){
-    tableBody.append('<tr></tr>');
+    table.append('<tbody></tbody>');
+    let tableBody = table.find('tbody');
+
+    for(let i =0; i<height;i++){
+      tableBody.append('<tr></tr>');
+    }
+
+    tableBody.find('tr').each(function(){
+      for(let j=0;j<weight;j++){
+        $(this).append('<td></td>');
+      }
+    });
+    // Your code goes here!
   }
 
-  tableBody.find('tr').each(function(){
-    for(let j=0;j<weight;j++){
-      $(this).append('<td></td>');
+  table.on('click', "td", function(){
+    var color = $('#colorPicker').val();
+
+    if($(this).attr('bgcolor')===color){
+      $(this).attr("bgcolor", '#fff');
+    }else{
+      $(this).attr("bgcolor", color);
     }
   });
-  // Your code goes here!
-}
 
-table.on('click', "td", function(){
-  var color = $('#colorPicker').val();
-
-  if($(this).attr('bgcolor')===color){
-    $(this).attr("bgcolor", '#fff');
-  }else{
-    $(this).attr("bgcolor", color);
-  }
 });
