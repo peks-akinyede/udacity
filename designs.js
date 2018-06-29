4107933bcf81a4363aa9cd4f43767cc0a78337e8
Theodore Olanipekun Akinyede <olanipekun.akinyede@ucdconnect.ie>
2018-06-29 13:22:57 +0100

    add comments

--- designs.js
+++ designs.js
@@ -1,49 +1,56 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
 
 $(document).ready(function(){
   var table = $('#pixelCanvas');
   var submitButton = $('input[type="submit"]');
-  var div = $('.view');
 
+  // When size is submitted by the user, call makeGrid()
   submitButton.on('click', function (e) {
     e.preventDefault();
     makeGrid();
   });
 
-  function makeGrid() {
-    var height = $('#inputHeight').val();
-    var weight = $('#inputWeight').val();
-    $('tbody').remove();
-
-    table.append('<tbody></tbody>');
-    let tableBody = table.find('tbody');
-
-    for(let i =0; i<height;i++){
-      tableBody.append('<tr></tr>');
-    }
-
-    tableBody.find('tr').each(function(){
-      for(let j=0;j<weight;j++){
-        $(this).append('<td></td>');
-      }
-    });
-    // Your code goes here!
-  }
-
+  //When cell is clicked, set it to the chosen color
   table.on('click', "td", function(){
     var color = $('#colorPicker').val();
-    var backgroundColor = div.css('background-color');
-    
+    var tableBackgroundColor = table.attr('bgcolor');
+
+    //Changes cell color to default color if there are two clicks
+    //Using the same color. Results in a color on/off like feature
     if($(this).attr('bgcolor')===color){
-      $(this).attr("bgcolor", backgroundColor);
+      $(this).attr("bgcolor", tableBackgroundColor);
     }else{
       $(this).attr("bgcolor", color);
     }
   });
 
 
+  function makeGrid() {
+    var height = $('#inputHeight').val();
+    var width = $('#inputWidth').val();
+    //Allows to reset the table
+    $('tbody').remove();
+
+    //Add extra styling with jQuery
+    table.css('margin-bottom','16px');
+    table.attr('bgcolor','white');
+
+    //Building the 'table' tree
+    table.append('<tbody></tbody>');
+    let tableBody = table.find('tbody');
+
+    //Make 'height' number of rows
+    for(let i =0; i<height;i++){
+      tableBody.append('<tr></tr>');
+    }
+
+    //Make 'width' number of columns
+    tableBody.find('tr').each(function(){
+      for(let j=0;j<width;j++){
+        $(this).append('<td></td>');
+      }
+    });
+    // Your code goes here!
+  }
 
 });
